c31b4c7674f86f399b5b956d9b0a976612e9834a
Olga Maunsell <olgamaunsell@yahoo.co.uk>
2018-03-18 21:28:35 +0000

    all CRUD actions working

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,20 +1,45 @@
 class AnimalsController < ApplicationController
+
+  def animal_params
+    params.require(:animal).permit([:name, :legs])
+  end
+
   def index
-    animals = Animal.all
-    render :json => animals
+    @animals = Animal.all
+    render :json => @animals.as_json(methods: :move)
     # respond_to do |format|
     #   format.html
-      # format.json{render :json => @animals}
+    # format.json{render :json => @animals}
     # end
   end
 
   def show
-    animal = Animal.find(params[:id])
-    render :json => animal
+    @animal = Animal.find(params[:id])
+    render :json => @animal.as_json(methods: :move)
   end
 
   def create
-    animal = Animal.create( params.require(:animal).permit([:name, :legs]))
+    animal = Animal.create(animal_params)
     render :json => animal
+  end
+
+  def update
+    animal = Animal.find(params[:id])
+
+    if animal.update_attributes(animal_params)
+      render :json => animal
+    else
+      render :json => {status: :update_failed}
+    end
+  end
+
+  def destroy
+    animal = Animal.find(params[:id])
+
+    if animal.destroy!
+      render :json => { status: :success  }
+    else
+      render :json => { status: :delete_failed }
+    end
   end
 end
